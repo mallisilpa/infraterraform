380948c0c9aeb62c02c7a974f3a76a55bb3d7965
mallisilpa <138866154+mallisilpa@users.noreply.github.com>
2023-08-15 18:10:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hashtek-terraform-state-bucket"
+    bucket = "myterraformbucket2023"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "hashtekTerraformStatelock"
