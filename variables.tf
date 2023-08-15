426fc90712fda9fe8092521d64ef30a562a458e8
mallisilpa <138866154+mallisilpa@users.noreply.github.com>
2023-08-15 21:00:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "mykey" 
+    default     =  "apsouth1mykey" 
 }
 
 variable "instance_type" { 
